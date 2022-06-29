d23d03b8a895c74a11462711cc874ae78b21f127
Benjamin McLean <mclean.ben@gmail.com>
2022-06-29 18:55:32 -0500

    Initial commit

---
+++ LtRt2LCRS/Program.cs
@@ -0,0 +1,14 @@
+﻿using AudioWorks.Api;
+using System;
+
+namespace LtRt2LCRS
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			string path = args.Where(arg => File.Exists(arg)).FirstOrDefault() ?? throw new FileNotFoundException();
+			AudioFile audioFile = new AudioFile(path);
+		}
+	}
+}
